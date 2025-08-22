b5342c8f7f3e572136c41c6ef7386640fd70dfb1
Bilkes Elias <bilkes199@gmail.com>
2025-08-22 19:48:49 +0300

    feat: preparing the model.py which holds the architecture of our model

--- model.py
+++ model.py
@@ -3,11 +3,71 @@ import json
 
 
 
-class Model:
-    def __init__(self):
-        """Initialize the model (can be replaced with real model loading)."""
-        self.network = None
-        pass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1, bias=False),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1, bias=False),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+    def forward(self, x): return self.block(x)
+
+class UpBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_ch, out_ch, 2, stride=2)
+        self.conv = ConvBlock(in_ch, out_ch)
+    def forward(self, x, skip):
+        x = self.up(x)
+        diffY = skip.size()[2] - x.size()[2]
+        diffX = skip.size()[3] - x.size()[3]
+        x = F.pad(x, [diffX // 2, diffX - diffX//2,
+                      diffY // 2, diffY - diffY//2])
+        return self.conv(torch.cat([skip, x], dim=1))
+
+class MultiTaskModel(nn.Module):
+    def __init__(self, in_channels=1, num_classes=3, base_ch=32):
+        super().__init__()
+        self.enc1 = ConvBlock(in_channels, base_ch)
+        self.pool1 = nn.MaxPool2d(2)
+        self.enc2 = ConvBlock(base_ch, base_ch*2)
+        self.pool2 = nn.MaxPool2d(2)
+        self.enc3 = ConvBlock(base_ch*2, base_ch*4)
+        self.pool3 = nn.MaxPool2d(2)
+        self.enc4 = ConvBlock(base_ch*4, base_ch*8)
+        self.pool4 = nn.MaxPool2d(2)
+        self.bottleneck = ConvBlock(base_ch*8, base_ch*16)
+        self.up4 = UpBlock(base_ch*16, base_ch*8)
+        self.up3 = UpBlock(base_ch*8, base_ch*4)
+        self.up2 = UpBlock(base_ch*4, base_ch*2)
+        self.up1 = UpBlock(base_ch*2, base_ch)
+        self.seg_head = nn.Conv2d(base_ch, 1, kernel_size=1)
+        self.cls_gap = nn.AdaptiveAvgPool2d((1,1))
+        self.cls_fc = nn.Linear(base_ch*16, num_classes)
+
+    def forward(self, x):
+        s1 = self.enc1(x); p1 = self.pool1(s1)
+        s2 = self.enc2(p1); p2 = self.pool2(s2)
+        s3 = self.enc3(p2); p3 = self.pool3(s3)
+        s4 = self.enc4(p3); p4 = self.pool4(s4)
+        b = self.bottleneck(p4)
+        d4 = self.up4(b, s4)
+        d3 = self.up3(d4, s3)
+        d2 = self.up2(d3, s2)
+        d1 = self.up1(d2, s1)
+        seg = self.seg_head(d1)
+        cls = self.cls_gap(b).flatten(1)
+        cls = self.cls_fc(cls)
+        return seg, cls
+
     
 
 
