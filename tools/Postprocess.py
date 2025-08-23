9a9a94f94360d04b19e01762826d37881f8d7253
Bilkes Elias <bilkes199@gmail.com>
2025-08-23 09:57:17 +0300

    feat: preparing the postprocess.py

--- tools/Postprocess.py
+++ tools/Postprocess.py
@@ -7,7 +7,10 @@ def postprocess_segmentation(seg_output):
     Postprocess segmentation output to create clean binary masks.
     
     """
-    return binary_mask
+    
+    seg_mask = torch.sigmoid(seg_output).cpu().numpy().squeeze()
+    
+    return seg_mask
 
 
 def postprocess_classification(cls_output):
@@ -15,8 +18,13 @@ def postprocess_classification(cls_output):
     Postprocess classification output to get predicted class.
     
     """
+    # Classification mapping
+    cls_map = {0: "Normal", 1: "Benign", 2: "Malignant"}
+    cls_idx = torch.argmax(cls_output, dim=1).item()
+    cls_label = cls_map[cls_idx]
+
    
-    return predicted_class
+    return cls_label
 
 
 
