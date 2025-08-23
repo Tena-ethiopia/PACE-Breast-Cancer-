6b72b0b105dde73ff5d70db686d2630e1fb74279
Bilkes Elias <bilkes199@gmail.com>
2025-08-23 15:32:52 +0300

    feat: adding the preprocess.py

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -1,10 +1,21 @@
 
     
+
+import torchvision.transforms as transforms
+
 def preprocess_segmentation(image):
     """
     Preprocess image for segmentation task.
     
     """
+    
+# Preprocessing
+    transform_img = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
+    ])
     pass
     
 def preprocess_classification(image):
@@ -12,4 +23,12 @@ def preprocess_classification(image):
     Preprocess image for classification task.
     
     """
+    
+# Preprocessing
+    transform_img = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
+    ])
     pass
